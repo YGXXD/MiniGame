3a53b52437384bf3f0f8d65e4c262c1fd13eca7c
YGXXD <20191120197@mail.ynu.edu.cn>
2022-10-11 16:27:45 +0800

    角色出生 和相机位置初始化

--- Source/MiniGame/GamePlayCore/GameMode/MiniGameGameModeBase.cpp
+++ Source/MiniGame/GamePlayCore/GameMode/MiniGameGameModeBase.cpp
@@ -2,6 +2,8 @@
 
 
 #include "MiniGameGameModeBase.h"
+#include "EngineUtils.h"
+#include "GameFramework/PlayerStart.h"
 #include "Kismet/GameplayStatics.h"
 #include "MiniGame/GamePlayCore/Character/MiniCharacter.h"
 #include "MiniGame/GamePlayCore/Controller/MiniController.h"
@@ -22,16 +24,41 @@ void AMiniGameGameModeBase::BeginPlay()
 	if(UWorld* ThisWord = GetWorld())
 	{
 		//生成共享相机
-		const FVector SpawnLocationCamera(0,0,0);
-		const FRotator SpawnRotationCamera(0,0,0);
-		FActorSpawnParameters ParametersCamera;
-		ParametersCamera.Owner = this;
-		ParametersCamera.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-		AShareCamera* ShareCamera = ThisWord->SpawnActor<AShareCamera>(AShareCamera::StaticClass(),SpawnLocationCamera,SpawnRotationCamera,ParametersCamera);
+		AShareCamera* ShareCamera = nullptr;
+		TActorIterator<AShareCamera> ShareCameraIt(ThisWord, AShareCamera::StaticClass());
+		if(ShareCameraIt)
+		{
+			ShareCamera = *ShareCameraIt;
+		}
+		else
+		{
+			const FVector SpawnLocationCamera(0,0,0);
+			const FRotator SpawnRotationCamera(0,0,0);
+			FActorSpawnParameters ParametersCamera;
+			ParametersCamera.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+			ShareCamera = ThisWord->SpawnActor<AShareCamera>(AShareCamera::StaticClass(),SpawnLocationCamera,SpawnRotationCamera,ParametersCamera);
+		}
 
+		//获取角色出生点
+		FVector SpawnLocationOne(0,0,100);
+		FRotator SpawnRotationOne(0,0,0);
+		FVector SpawnLocationTwo(20,20,100);
+		FRotator SpawnRotationTwo(0,0,0);
+		for(TActorIterator<APlayerStart> PlayerStartIt(ThisWord, APlayerStart::StaticClass()); PlayerStartIt; ++PlayerStartIt)
+		{
+			if((*PlayerStartIt)->PlayerStartTag == TEXT("One"))
+			{
+				SpawnLocationOne = (*PlayerStartIt)->GetActorLocation();
+				SpawnRotationOne = (*PlayerStartIt)->GetActorRotation();
+			}
+			else if((*PlayerStartIt)->PlayerStartTag == TEXT("Two"))
+			{
+				SpawnLocationTwo = (*PlayerStartIt)->GetActorLocation();
+				SpawnRotationTwo = (*PlayerStartIt)->GetActorRotation();
+			}
+		}
+		
 		//生成角色1
-		const FVector SpawnLocationOne(0,0,100);
-		const FRotator SpawnRotationOne(0,0,0);
 		FActorSpawnParameters ParametersOne;
 		ParametersOne.Owner = UGameplayStatics::GetPlayerController(ThisWord,0);
 		ParametersOne.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
@@ -40,8 +67,6 @@ void AMiniGameGameModeBase::BeginPlay()
 		UGameplayStatics::GetPlayerController(ThisWord,0)->Possess(CharacterOne);
 
 		//生成角色2
-		const FVector SpawnLocationTwo(20,20,100);
-		const FRotator SpawnRotationTwo(0,0,0);
 		FActorSpawnParameters ParametersTwo;
 		ParametersTwo.Owner = UGameplayStatics::GetPlayerController(ThisWord,1);
 		ParametersTwo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
